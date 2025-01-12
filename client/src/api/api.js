272d06ceaf285da91d76cdc086cb3085bc0de1c3
sagarkumar82 <sagarkumar0802001@gmail.com>
2025-01-12 16:22:21 +0530

    change build

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: import.meta.env.VITE_BASE_URL });
+const API = axios.create({ baseURL: 'https://tool-management.onrender.com/api' });
 
 export const registerMechanic = async (data) => {
   try {
